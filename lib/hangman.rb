e15993ca56c65831fba27fef2ad2d0a08a8bb6f0
Alan Sobchacki <a.sobchack@gmail.com>
2023-10-09 18:46:52 -0300

    Will add a responsive, old-school hangman drawing.

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,18 +1,13 @@
 
 class Hangman
   def initialize
-    @random_word = ''
     @hints = []
     @failed_tries = 0
-  end
-
-  def hangman_drawing
-    puts '   ______'
-    puts '   |    |'
-    puts '   O    |'
-    puts '  /|\   |'
-    puts '  / \   |'
-    puts '        |'
+    @random_word = ''
+    @hangman_rope = ' '
+    @hangman_head = ' '
+    @hangman_upper_body = '   '
+    @hangman_lower_body = '   '
   end
 
   def pick_game_difficulty
@@ -32,27 +27,60 @@ class Hangman
   def generate_word_and_hints(min_length, max_length)
     @random_word = File.readlines('10000_words.txt').sample.chomp until @random_word.length.between?(min_length, max_length)
     @hints.push('_') until @hints.length == @random_word.length
-    p @random_word
-    p @hints
   end
 
   def game_running
-    puts '  Guess a letter!'
+    update_display
     @guess = gets.chomp
+
     @random_word.each_char.with_index do |char, index|
-      if @guess == char then update_hints(index, @guess)
-      end
+      update_hints(index, @guess) unless @guess != char
     end
 
+    @failed_tries += 1 unless @random_word.include?(@guess) == true
+
+    update_failed_tries
     game_running
   end
+
+  # The methods below will only handle interface updates
 
   def update_hints(index, letter)
     @hints.delete_at(index)
     @hints.insert(index, letter)
   end
+
+  def update_failed_tries
+    case @failed_tries
+    when 1 then @hangman_rope = '|'
+    when 2 then @hangman_head = 'O'
+    when 3 then @hangman_upper_body = '/  '
+    when 4 then @hangman_upper_body = '/| '
+    when 5 then @hangman_upper_body = '/|\\'
+    when 6 then @hangman_lower_body = '/  '
+    when 7 then @hangman_lower_body = '/ \\'
+    end
+  end
+
+  def hangman_drawing
+    puts '       ______'
+    puts "       #{@hangman_rope}    |"
+    puts "       #{@hangman_head}    |"
+    puts "      #{@hangman_upper_body}   |"
+    puts "      #{@hangman_lower_body}   |"
+    puts '            |'
+  end
+
+  def update_display
+    hangman_drawing
+    puts ''
+    puts @hints.join(' ')
+    puts ''
+    puts '  Guess a letter!'
+  end
 end
 
 playing_hangman = Hangman.new
+playing_hangman.hangman_drawing
 playing_hangman.pick_game_difficulty
 playing_hangman.game_running
